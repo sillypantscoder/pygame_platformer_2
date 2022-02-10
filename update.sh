0236b0939db52bba00dcb7ddd49c7d2c94c266f4
Jason Roman <roman.jason29@svvsd.org>
2022-02-10 14:21:40 -0700

    Updated updater script to handle conflicting changes

--- update.sh
+++ update.sh
@@ -1,11 +1,20 @@
 #!/usr/bin/env bash
+status=$(git status -s);
+
+tfind=$"\n M";
+
+if [[ $"\n$status" == *"$tfind"* ]]; then
+	echo "WARNING: There are changes in the current directory that may conflict with incoming changes:"
+	git status -s
+	echo
+fi
 
 echo "Updates available:\n"
 git fetch -q && git log --no-decorate --reverse --pretty=%C\(bold\)Update:\ %C\(red\)%s%n\ \ \ \ \ \ \ \ %b ..origin/main
 echo
 read -p "Continue (y/n)? " choice
 case "$choice" in 
-  y|Y ) echo "Downloading updates..."; git pull -q; echo "Done!";;
-  n|N ) echo "Canceled.";;
-  * ) echo "Invalid response; canceled.";;
+	y|Y ) echo "Downloading updates..."; git pull -q; echo "Done!";;
+	n|N ) echo "Canceled.";;
+	* ) echo "Invalid response; canceled.";;
 esac
